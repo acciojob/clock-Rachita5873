e63c9d9cf3cf7a6ef04618db2efd131377de9d17
Rachita5873 <76832946+Rachita5873@users.noreply.github.com>
2023-08-22 13:37:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+var val = document.getElementById("timer");
+let today = new Date();
+let month = today.getMonth()+1;
+let year = today.getFullYear();
+let date = today.getDate();
+let currentDate = month + "/" + date + "/" + year+",";
+let hours = today.getHours();
+let minutes = today.getMinutes();
+let seconds = today.getSeconds();
+if(minutes < 10){
+	minutes = "0"+minutes;
+}
+
+
+if(seconds < 10){
+	seconds = "0"+seconds;
+}
+
+var currTime = currentDate +  " " +hours + ":" + minutes + ":" + seconds;
+
+
+
+val.innerText = currTime
+
+// val.innerText = currTime;
+
+
+
+
